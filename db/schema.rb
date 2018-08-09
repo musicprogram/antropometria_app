42ec8ea5ba5a06042290481cd7a4f87d2d6629f8
musicprogram <juan-o-henao@hotmail.es>
2018-08-09 06:32:03 -0500

    cambiando videos origen

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180720202515) do
+ActiveRecord::Schema.define(version: 20180720201934) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "measurements", force: :cascade do |t|
     t.float "bust"
